2e99ac63d85ff1cec3146336ea3efdebd9b633a9
Thomas Ridings <thomas.a.ridings@gmail.com>
2021-09-02 23:30:34 +0100

    accessibility improvement mobile menu button aria

--- src/components/Menu.svelte
+++ src/components/Menu.svelte
@@ -14,7 +14,7 @@
     </span>
 
     <!-- mobile menu button -->
-    <button class="mobile-menu-button p-4 focus:outline-none focus:bg-gray-700" on:click="{() => hidden = !hidden}">
+    <button class="mobile-menu-button p-4 focus:outline-none focus:bg-gray-700" on:click="{() => hidden = !hidden}" aria-label="open mobile menu">
       <svg class="h-5 w-5 {!hidden ? 'hidden' : ''}" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
       </svg>
