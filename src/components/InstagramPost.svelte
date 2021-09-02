e735afb07b13aebad0adee25834fdeeb9eab72e7
Thomas Ridings <thomas.a.ridings@gmail.com>
2021-09-02 23:28:58 +0100

    Fixing noopener isseus with Insta links

--- src/components/InstagramPost.svelte
+++ src/components/InstagramPost.svelte
@@ -4,6 +4,6 @@
   export let caption;
 </script>
 
-<a class="w-full lg:w-1/4 p-2 hover:brightness-90 rounded-sm" href={link} target="_blank">
+<a class="w-full lg:w-1/4 p-2 hover:brightness-90 rounded-sm" href={link} target="_blank" rel="noopener">
   <img src={image} class="cover h-full w-full" alt={caption}>
 </a>
