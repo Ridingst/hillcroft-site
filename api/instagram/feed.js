8d5100a5b231d3f57ce67bfdfeac5d794c5dcb76
Thomas Ridings <thomas.a.ridings@gmail.com>
2021-09-02 21:12:08 +0100

    Instagram Feed API test

--- api/instagram/feed.js
+++ api/instagram/feed.js
@@ -1,15 +1,20 @@
 const axios = require('axios');
-import { TokenFileWebIdentityCredentials } from 'aws-sdk';
 import { retrieveValidToken } from './tokenManagement.js'
 
 module.exports = (req, res) => {
   retrieveValidToken()
   .then(token => {
-    return axios.get('https://graph.instagram.com/me?fields=id,username&'+token.access_token)
+    console.log('querying Instagram API')
+    return axios.get('https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,permalink,thumbnail_url&access_token='+token.access_token)
   })
-  .then(response => res.send(response.data))
+  .then(response => {
+    // The response comes in {data: 0: post0Info, 1: post1Info...} and we need an array
+    // We also filter to 8 items as that's all we want to display
+    return [...Array(8).keys()].map(p=> response.data[p])
+  })
+  .then(posts => res.send(posts))
   .catch(err => {
-    console.error(err)
+    console.error(err.response)
     res.status(500)
     res.send("Error")
   })
