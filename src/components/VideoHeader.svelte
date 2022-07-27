a5b1f3297845ef0c18979fb748ffadae2fcf1d5c
Thomas Ridings <thomas.a.ridings@gmail.com>
2022-07-28 00:22:40 +0100

    Fixing header page

--- src/components/VideoHeader.svelte
+++ src/components/VideoHeader.svelte
@@ -1,7 +1,7 @@
 <header class="relative lg:flex items-center justify-center h-screen w-full overflow-hidden top-0 bg-black hidden max-h-screen px-8">
   <div class="absolute z-10 bg-opacity-60 bg-black w-full h-full"></div>
   <div class="relative z-20 p-5 text-2xl text-white w-full">
-    <h1 class="text-4xl xl:text-7xl tracking-widest font-bold uppercase leading-snug">CI Demo Example 2<span class="text-yellow-500">.</span></h1>
+    <h1 class="text-4xl xl:text-7xl tracking-widest font-bold uppercase leading-snug">Hillcroft Lacrosse Club<span class="text-yellow-500">.</span></h1>
     <h2 class="text-4xl xl:text-7xl tracking-widest font-bold uppercase leading-snug">South London</h2>
     <h2 class="text-xl xl:text-2xl tracking-wider uppercase py-3">MENS • WOMENS • SOCIAL</h2>
   </div>
