6da4f40de2db482198d278393ce27e4c073644e1
Thomas Ridings <thomas.a.ridings@gmail.com>
2022-07-30 11:06:06 +0100

    Switching from local file to s3 location

--- api/getVenues.js
+++ api/getVenues.js
@@ -4,7 +4,8 @@ import { number } from 'yup';
 var csv = require('csvtojson');
 
 export default function handler(req, res) {
-  const file = path.join(process.cwd(), 'files', 'data.csv');
+  //const file = path.join(process.cwd(), 'files', 'data.csv');
+  file = 'https://hillcroft-lacrosse.s3.eu-west-1.amazonaws.com/data.csv'
   csv({id:"number", lat: "number", lng: "number"})
   .fromFile(file)
   .then(function(jsonArrayObj){ 
