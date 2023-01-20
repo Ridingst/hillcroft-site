39e04ea60ce50a40e3c1010418eb34ef53f841cb
Pedro Gomes <23638870+sh1n3sec@users.noreply.github.com>
2023-01-20 15:15:07 +0000

    Update with new Hub's location

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -16,10 +16,10 @@
           <div class="text-sm text-gray-700 text-center">
             <div class="pb-4">
               <p tabindex="0" class="text-gray-700">Hillcroft Lacrosse Club</p>
-              <p>Millennium Arena</p>
-              <p>Battersea Park, East Carriage Drive</p>
+              <p>City of London Academy Southwark</p>
+              <p>240 Lynton Rd</p>
               <p>London</p>
-              <p>SW11 4NJ</p>
+              <p>SE1 5LA</p>
             </div>
             <p tabindex="0" class="text-gray-700">Built by <a href="https://thomasridings.com">Thomas Ridings</a>.</p>
           </div>
